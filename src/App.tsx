3186c98f92c26ca7671f519140f11f98468d82e1
ngortiz <nidigortiz21@gmail.com>
2024-10-21 14:29:24 -0300

    a new project is created

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './Router';
+import { NotificationProvider } from './context/notification.context';
+import { Suspense } from 'react';
+
+function App() {
+  return (
+    <NotificationProvider>
+      <BrowserRouter>
+        <Suspense fallback={'Cargando...'}>
+          <AppRouter />
+        </Suspense>
+      </BrowserRouter>
+    </NotificationProvider>
+  );
+}
+
+export default App;
